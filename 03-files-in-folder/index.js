bb7510d477527dde7dd9e463ecc681848159ce1a
Ulistonee <ulistonee@gmail.com>
2024-11-26 14:44:19 +0500

    feat: files in folder task done

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const secretFolderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(secretFolderPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error('Error reading the folder:', err.message);
+    return;
+  }
+
+  files.forEach((file) => {
+    if (file.isFile()) {
+      const filePath = path.join(secretFolderPath, file.name);
+
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.error('Error getting file stats:', err.message);
+          return;
+        }
+
+        const { name, ext } = path.parse(file.name);
+        const sizeInKB = stats.size / 1024;
+
+        console.log(`${name} - ${ext.slice(1)} - ${sizeInKB.toFixed(3)}kb`);
+      });
+    } else {
+      console.error(`Error: "${file.name}" is not a file.`);
+    }
+  });
+});
